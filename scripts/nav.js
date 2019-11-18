b06b2efe0eb6d380fc5a02c8e17164a544982f71
VoidedRL <geraintps@gmail.com>
2019-11-18 12:37:14 +0000

    Working Nav Menu

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,26 +1,14 @@
-Array.from(document.getElementsByTagName('path')).map(function (path) {
-  console.log(path.getTotalLength());
-  var debugPath = path.cloneNode();
-  debugPath.classList.add('line--debug');
-  if (path.parentNode)
-  path.parentNode.insertBefore(debugPath.cloneNode(), path);
-});
-var debugCheckbox = document.getElementById('debug');
-debugCheckbox.addEventListener('change', function () {
-  if (debugCheckbox.checked) {
-    debugCheckbox.parentElement.classList.add('active');
-  } else
-  {
-    debugCheckbox.parentElement.classList.remove('active');
-  }
-});
-var currentActive = 0;
-var checkboxes = document.querySelectorAll('.grid input');
-var autoShow = setInterval(function () {
-  checkboxes[currentActive % 4].checked = !checkboxes[currentActive % 4].checked;
-  if (!checkboxes[currentActive % 4].checked)
-  currentActive += 1;
-}, 1000);
-document.querySelector('.grid').addEventListener('click', function () {
-  clearInterval(autoShow);
-});
+'use strict';
+
+(function() {
+  var body = document.body;
+  var burgerMenu = document.getElementsByClassName('b-menu')[0];
+  var burgerContain = document.getElementsByClassName('b-container')[0];
+  var burgerNav = document.getElementsByClassName('b-nav')[0];
+
+  burgerMenu.addEventListener('click', function toggleClasses() {
+    [body, burgerContain, burgerNav].forEach(function (el) {
+      el.classList.toggle('open');
+    });
+  }, false);
+})();
