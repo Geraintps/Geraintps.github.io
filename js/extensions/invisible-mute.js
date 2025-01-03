375d923165e7b8c9cc5bc31de955cc0191828802
Geraint Pickard-Skeats <geraintps@gmail.com>
2025-01-03 00:52:29 +0000

    Logging

--- js/extensions/invisible-mute.js
+++ js/extensions/invisible-mute.js
@@ -1,7 +1,10 @@
 invisible-mute.js text/javascript
+console.log("Invisible Mute Extension Loaded");
 // non-jquery version
 // on document load
 document.addEventListener('DOMContentLoaded', function() {
+
+	console.log("Invisible Mute Extension Active");
 
 	// get the mute button (button element with data-a-target="player-mute-unmute-button")
 	var muteButton = document.querySelector('button[data-a-target="player-mute-unmute-button"]');
@@ -9,8 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	// if the mute button exists
 	if (muteButton) {
 
+		console.log("Mute Button Found");
+
 		// when the tab is unfocused, mute the player
 		document.addEventListener('visibilitychange', function() {
+
+			console.log("Visibility Change");
+
 			if (document.hidden) {
 
 				// if the aria-label is "Mute (m)", click the button
