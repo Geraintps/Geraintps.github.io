3ffd8846b829eacae5bc340db57133d2dc039966
Geraint Pickard-Skeats <geraintps@gmail.com>
2025-01-03 00:58:06 +0000

    Fixes

--- js/extensions/invisible-mute.js
+++ js/extensions/invisible-mute.js
@@ -1,37 +1,52 @@
 invisible-mute.js text/javascript
-console.log("Invisible Mute Extension Loaded");
 // non-jquery version
-// on document load
-document.addEventListener('DOMContentLoaded', function() {
+(function() {
 
 	console.log("Invisible Mute Extension Active");
 
-	// get the mute button (button element with data-a-target="player-mute-unmute-button")
-	var muteButton = document.querySelector('button[data-a-target="player-mute-unmute-button"]');
+	// if the hostname is not twitch.tv, return
+	if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
 
-	// if the mute button exists
-	if (muteButton) {
-
-		console.log("Mute Button Found");
-
-		// when the tab is unfocused, mute the player
-		document.addEventListener('visibilitychange', function() {
-
-			console.log("Visibility Change");
-
-			if (document.hidden) {
-
-				// if the aria-label is "Mute (m)", click the button
-				if (muteButton.getAttribute('aria-label') === 'Mute (m)') {
-					muteButton.click();
-				}
-			} else {
-
-				// if the aria-label is "Unmute (m)", click the button
-				if (muteButton.getAttribute('aria-label') === 'Unmute (m)') {
-					muteButton.click();
-				}
-			}
-		});
+	// if the document is already loaded, call onLoad
+	if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
+        onLoad();
+    } else {
+        window.addEventListener("DOMContentLoaded", function() {
+            onLoad();
+        });
 	}
-});
+
+	function onLoad() {
+
+		console.log("Invisible Mute Extension Loaded");
+
+		// get the mute button (button element with data-a-target="player-mute-unmute-button")
+		var muteButton = document.querySelector('button[data-a-target="player-mute-unmute-button"]');
+
+		// if the mute button exists
+		if (muteButton) {
+
+			console.log("Mute Button Found");
+
+			// when the tab is unfocused, mute the player
+			document.addEventListener('visibilitychange', function() {
+
+				console.log("Visibility Change");
+
+				if (document.hidden) {
+
+					// if the aria-label is "Mute (m)", click the button
+					if (muteButton.getAttribute('aria-label') === 'Mute (m)') {
+						muteButton.click();
+					}
+				} else {
+
+					// if the aria-label is "Unmute (m)", click the button
+					if (muteButton.getAttribute('aria-label') === 'Unmute (m)') {
+						muteButton.click();
+					}
+				}
+			});
+		}
+	}
+})();
